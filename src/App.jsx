e134d16e6ca036b5d7081b72aa953c517b60037f
Farhad <farkhad0527@gmail.com>
2025-12-31 17:40:37 +0500

    Files

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
                 <Header />
 
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/space-tourism-web/" element={<Home />} />
                     <Route path="/destination" element={<Destination />} />
                     <Route path="/crew" element={<Crew />} />
                     <Route path="/technology" element={<Technology />} />

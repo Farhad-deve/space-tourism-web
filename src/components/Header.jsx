5c2b1c43d39fca9ebc976983c98c57f28467a3f5
Farhad <farkhad0527@gmail.com>
2025-12-31 18:09:58 +0500

    Files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,16 +13,16 @@ const Header = () => {
         <header className="pos-a width-100">
             <div className="d-flex align-i-center just-c-sb pad-1-5r">
                 <div>
-                    <img src="/assets/shared/logo.svg" alt="Logo" />
+                    <img src="assets/shared/logo.svg" alt="Logo" />
                 </div>
 
                 <div className="pos-a line hide-mob"></div>
 
                 <div>
                     <nav>
-                        <img src="/assets/shared/icon-hamburger.svg" alt="" className="hide-des" onClick={() => setMenuOpen(true)} />
+                        <img src="assets/shared/icon-hamburger.svg" alt="" className="hide-des" onClick={() => setMenuOpen(true)} />
                         <ul className={"d-flex align-i-center gap-2r font-f-Bar-Con lett-sp-2 word-sp-05r menu tr-a-03s-ea flex-column-mob align-i-st-mob" + (menuOpen ? ' active' : '')}>
-                            <img src="/assets/shared/icon-close.svg" alt="" className="pos-a icon-close hide-des" onClick={() => setMenuOpen(false)} />
+                            <img src="assets/shared/icon-close.svg" alt="" className="pos-a icon-close hide-des" onClick={() => setMenuOpen(false)} />
                             <Link to="/" className="color-w">
                                 <li onClick={() => location.pathname === '/'} className={"pad-tb-2-5r tr-a-03s-ea" + (location.pathname === '/' ? ' active-nav' : ' inactive-nav')}><b>00</b> HOME</li>
                             </Link>
